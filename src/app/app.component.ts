8757418def1014dcd030f9620360bfa8628ad798
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-03-25 12:36:48 -0500

    feat: atributes directive highlight was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {HighLightDirective} from '@app/directives/HighLight.directive'
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    HighLightDirective,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-primeng';
+  title = 'angular-course';
 }
