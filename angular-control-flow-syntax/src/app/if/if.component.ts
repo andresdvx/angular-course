8678f0da69b1120a643b7ab9af55c8caadb84d7a
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-03-27 11:29:03 -0500

    add: control flow syntax NgId => @If

--- angular-control-flow-syntax/src/app/if/if.component.ts
+++ angular-control-flow-syntax/src/app/if/if.component.ts
@@ -1,12 +1,11 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-if',
-  imports: [NgIf],
+  imports: [],
   templateUrl: './if.component.html',
   styleUrl: './if.component.scss'
 })
 export class IfComponent {
-  protected isVisible : boolean = true;
+  protected isVisible : boolean = false;
 }
