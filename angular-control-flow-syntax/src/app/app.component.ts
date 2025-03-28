d029814b20261e82204342e8ef4125f4d15756d5
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-03-28 15:40:06 -0500

    add: @switch control flow - prev and new syntax

--- angular-control-flow-syntax/src/app/app.component.ts
+++ angular-control-flow-syntax/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoadingComponent } from './loading/loading.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ LoadingComponent ],
+  imports: [ SwitchComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
