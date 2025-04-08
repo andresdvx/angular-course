5646f6a130aa2a32edb279c8f1d7a5b35cca9978
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-04-07 22:25:59 -0500

    add:

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+
+interface ItemForm {
+  id: FormControl<number>;
+  name: FormControl<string>;
+  value: FormControl<number>;
+}
+
+export type CustomFormGroup = FormGroup<ItemForm>;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-forms';
+  protected fb = inject(NonNullableFormBuilder);
+  protected form: FormGroup<{ items: FormArray<CustomFormGroup> }> =
+    this.fb.group({
+      items: this.fb.array<CustomFormGroup>([]),
+    });
+  protected items = signal(this.form.controls.items);
+
+  addItems() {
+    const id = this.items.length + 1;
+    const itemForm = this.fb.group<ItemForm>({
+      id: this.fb.control(id),
+      name: this.fb.control('', {validators: [Validators.required]}),
+      value: this.fb.control(0, {validators: [Validators.required]}),
+    })
+    this.form.controls.items.push(itemForm);
+    this.items.set(this.form.controls.items);
+  }
 }
